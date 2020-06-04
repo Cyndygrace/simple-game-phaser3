1cb9729eef683d36d710a917b4ffe204227efe28
cynthia iloekwe <cyniloekwe@gmail.com>
2020-06-04 19:40:28 +0100

    add collission object and cause interaction with hero

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -35,6 +35,13 @@ class Game extends Phaser.Scene {
     });
     // call hero method with the arguments
     this.hero = new Hero(this, 250, 160);
+    // provide a platform for the hero to fall from
+    // we use a rectangle method and specify x and y position with breath and height amd color.
+    const platform = this.add.rectangle(220, 240, 260, 10, 0x4bcb7c);
+    // add a physics body with static to true, which means it cannot be moved by other objects
+    this.physics.add.existing(platform, true);
+    //cause a collision interaction between the rectangle and hero
+    this.physics.add.collider(this.hero, platform);
   }
 
   // calls the method 60 times per sec.
