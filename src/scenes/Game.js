e3eeb56f6ca319ff99e619fc4d733f5646e5c6fe
cynthia iloekwe <cyniloekwe@gmail.com>
2020-06-05 12:06:47 +0100

    added animations to new sprites

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -50,9 +50,56 @@ class Game extends Phaser.Scene {
     // create animmation for the sprite-sheet
     this.anims.create({
       // key for refernce in other files
+      key: 'hero-idle',
+      // frame to include all the sprites that forms idle on the spirte sheet (only one frame here)
+      frames: this.anims.generateFrameNumbers('hero-idle-sheet'),
+    });
+    // create animmation for the sprite-sheet
+    this.anims.create({
+      // key for refernce in other files
       key: 'hero-running',
       // frame to include all the sprites that forms running on the spirte sheet
       frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      // tells phaser to display 10 frames per sec
+      frameRate: 10,
+      // keep playing continously
+      repeat: -1,
+    });
+    // create animmation for the sprite-sheet
+    this.anims.create({
+      // key for refernce in other files
+      key: 'hero-pivoting',
+      // frame to include all the sprites that forms pivoting on the spirte sheet
+      frames: this.anims.generateFrameNumbers('hero-pivot-sheet'),
+    });
+    // create animmation for the sprite-sheet
+    this.anims.create({
+      // key for refernce in other files
+      key: 'hero-jumping',
+      // frame to include all the sprites that forms jumping on the spirte sheet
+      frames: this.anims.generateFrameNumbers('hero-jump-sheet'),
+      // tells phaser to display 10 frames per sec
+      frameRate: 10,
+      // keep playing continously
+      repeat: -1,
+    });
+    // create animmation for the sprite-sheet
+    this.anims.create({
+      // key for refernce in other files
+      key: 'hero-flipping',
+      // frame to include all the sprites that forms flipping on the spirte sheet
+      frames: this.anims.generateFrameNumbers('hero-flip-sheet'),
+      // tells phaser to display 10 frames per sec
+      frameRate: 30,
+      // keep playing continously
+      repeat: -1,
+    });
+    // create animmation for the sprite-sheet
+    this.anims.create({
+      // key for refernce in other files
+      key: 'hero-falling',
+      // frame to include all the sprites that forms falling on the spirte sheet
+      frames: this.anims.generateFrameNumbers('hero-fall-sheet'),
       // tells phaser to display 10 frames per sec
       frameRate: 10,
       // keep playing continously
